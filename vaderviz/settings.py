8517f479ded43c338ee708f17dc44f7b684a6e5c
Ben Draffin <bdraffin@sei.cmu.edu>
2016-09-27 13:00:39 -0700

    updated routes. Demo is now at http://localhost:8000/networkviz/demo/

--- vaderviz/settings.py
+++ vaderviz/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'networkviz.apps.NetworkvizConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
