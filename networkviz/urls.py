eeb08f88f80d179a382436a0a62a90d5c0271094
Ben Draffin <bdraffin@sei.cmu.edu>
2016-09-27 17:07:59 -0700

    Updated Visualization to add interaction

--- networkviz/urls.py
+++ networkviz/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^demo/', views.demo, name='demo'),
+    url(r'^medium/', views.medium, name='medium'),
+    url(r'^cmu/', views.cmu, name='cmu'),
+    url(r'^d3/', views.d3, name='d3'),
+    url(r'^ieee123/', views.ieee123, name='ieee123'),
 ]
--- networkviz/views.py
+++ networkviz/views.py
@@ -15,3 +15,23 @@ def demo(request):
     template = loader.get_template('networkviz/simple.html')
     context = {}
     return HttpResponse(template.render(context, request))
+
+def medium(request):
+    template = loader.get_template('networkviz/medium.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def cmu(request):
+    template = loader.get_template('networkviz/cmu.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def d3(request):
+    template = loader.get_template('networkviz/d3.js')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def ieee123(request):
+    template = loader.get_template('networkviz/ieee123.xml')
+    context = {}
+    return HttpResponse(template.render(context, request))
