077cf65bc1183318fcf718324f893ced519a0073
EasonX <yixinx@andrew.cmu.edu>
2016-09-30 22:12:10 -0700

    Add filtering sidebar, finish toggle button

--- networkviz/static/js/map.js
+++ networkviz/static/js/map.js
@@ -1,6 +1,6 @@
 var data;
 var node_size = 110;
-var mymap = L.map('mapid').setView([37.428096, -122.179255], 14);
+var mymap = L.map('energy-map').setView([37.428096, -122.179255], 14);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=sk.eyJ1IjoiYmVuZHJhZmZpbiIsImEiOiJjaXRtMnM3cjUwMGY2MnRwY3loODYybm02In0.b4HQeNWffCU4Q0CE6gynMw', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
