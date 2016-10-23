9075375288600e2e72a9ed0b3e2864945741ac1c
lqshan <lqshan1993@gmail.com>
2016-10-22 19:19:27 -0700

    Add some helper function.

--- networkviz/utilities/helper.py
+++ networkviz/utilities/helper.py
@@ -1,2 +1,6 @@
-def convert_decimal_list_to_string(input_arg):
-    return [str(x[0]) for x in input_arg]
+def convert_decimal_list_to_string(input_arg, index):
+    return [str(x[index]) for x in input_arg]
+
+
+def convert_decimal_list_to_float(input_arg, index):
+    return [float(x[index]) for x in input_arg]
