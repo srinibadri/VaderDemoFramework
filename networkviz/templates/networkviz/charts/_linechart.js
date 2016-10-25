0875ec3ffc51e17b1cfc316924029727c6f33692
Binglei Du <bdu@andrew.cmu.edu>
2016-10-25 01:23:43 -0700

    Add more statistics to heading panel

--- networkviz/templates/networkviz/charts/_linechart.js
+++ networkviz/templates/networkviz/charts/_linechart.js
@@ -75,9 +75,47 @@ $(document).ready(function() {
     y.domain([0, d3.max(supply, function(d) { return d.y; })*1.05]);
     svg.append('path').datum(supply).attr('class', 'supply').attr('d', line);
 
-    $('#total-power-generation').replaceWith('<div id = "total-power-generation" class="count">' + addCommas(Math.floor(supply[0].y)) + ' <small><small><small>kW</small></small></small></div>');
-    $('#total-demand').replaceWith('<div id = "total-power-generation" class="count">' + addCommas(Math.floor(demand[0].y)) + ' <small><small><small>kW</small></small></small></div>');
+    // heading panel statistics
+    var curEff = supply[0].y * 100 / demand[0].y;
+    var preEff = supply[1].y * 100 / demand[1].y;
+    $('.generation-stat').html(addCommas(Math.floor(supply[0].y)));
+    $('.demand-stat').html(addCommas(Math.floor(demand[0].y)));
+    $('.efficiency-stat').html(Math.floor(curEff));
+    var powerMax = supply[0].y, powerMin = supply[0].y, dateMax = supply[0].x, dateMin = supply[0].x;
+    for (var i = 1; i < supply.length; i++) {
+        if (supply[i].y >= powerMax) {
+            powerMax = supply[i].y;
+            dateMax = supply[i].x;
+        }
+        if (supply[i].y <= powerMin) {
+            powerMin = supply[i].y;
+            dateMin = supply[i].x;
+        }
+    }
+    $('.max-generation').html(addCommas(Math.floor(powerMax)));
+    $('.min-generation').html(addCommas(Math.floor(powerMin)));
+    $('.generation-inc').html('<i class="' + (supply[0].y > supply[1].y ? 'green' : 'red') + '"><i class="fa fa-sort-' + (supply[0].y > supply[1].y ? 'asc' : 'desc') + '"></i>' + Math.ceil((Math.abs(supply[0].y-supply[1].y) * 100 / supply[1].y))+ '% </i> From last 15 minutes')
+    $('.demand-inc').html('<i class="' + (demand[0].y > demand[1].y ? 'green' : 'red') + '"><i class="fa fa-sort-' + (demand[0].y > demand[1].y ? 'asc' : 'desc') + '"></i>' + Math.ceil((Math.abs(demand[0].y-demand[1].y) * 100 / demand[1].y))+ '% </i> From last 15 minutes')
+    $('.efficiency-inc').html('<i class="' + (curEff > preEff ? 'green' : 'red') + '"><i class="fa fa-sort-' + (curEff > preEff ? 'asc' : 'desc') + '"></i>' + Math.ceil((Math.abs(curEff-preEff) * 100 / preEff))+ '% </i> From last 15 minutes')
+    $('.max-generation-time').html('<i class="fa fa-clock-o"></i> ' + formatDate(dateMax));
+    $('.min-generation-time').html('<i class="fa fa-clock-o"></i> ' + formatDate(dateMin));
+
 });
+
+function formatDate(date) {
+    months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var date = new Date(date);
+    // Hours part from the timestamp
+    var hours = date.getHours();
+    // Minutes part from the timestamp
+    var minutes = '0' + date.getMinutes();
+    // Seconds part from the timestamp
+    var seconds = '0' + date.getSeconds();
+    // Will display time in 10:30:23 format
+    var formattedTime = months[date.getMonth()] + '.' + date.getDate() + ' '
+                        + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    return formattedTime;
+}
 
 function addCommas(nStr) {
     nStr += '';
