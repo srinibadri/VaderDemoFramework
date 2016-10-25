c0e5f9b4e54c49bbf9279542bef87a0feaf36da0
EasonX <yixinx@andrew.cmu.edu>
2016-10-24 23:09:35 -0700

    init.js, not finished yet

--- networkviz/static/js/init.js
+++ networkviz/static/js/init.js
@@ -1,15 +1,20 @@
 $(document).ready(function(){
     var $simulationList = ['ieee123', 'ieee123s', 'ieee123z', 'ieee123zs'];
     var $simulationName = $('#simulation-name').text();
-    
-    $('.dropdown').click(function(){
+
+    $('.dropdown-toggle').click(function(){
         var $options = "";
         for (var i = 0; i < $simulationList.length; i++){
             if ($simulationName != $simulationList[i]){
-                $options += '<li><a class="change-model">' + $simulationList[i] + '</a></li>';
+                $options += '<li><a>' + $simulationList[i] + '</a></li>';
                 $('.dropdown-menu').html($options);
             }
         }
     });
-    
+
+    $(document).on('click', '.dropdown-options li a', function () {
+        var $changeModel = $(this).text();
+        // Send AJAX to reload the whole page
+    });
+
 });
